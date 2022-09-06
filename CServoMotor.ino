815ba02f91d7c8212f4c8fb3bcb7a910e47307b3
Gabriel Assunção <79586454+Gabriel200107@users.noreply.github.com>
2022-09-06 18:07:42 -0300

    Add files via upload

---
+++ CServoMotor.ino
@@ -0,0 +1,25 @@
+/*   Código do Servo Motor Arduino que faz conexão direta com o arduino e com node-red que a partir de um dashboard criado por nós apertamos o botão 180 ele faz uma 
+rotação no servo motor e apertamos o de 0 graus ele volta.
+*/
+
+Servo meuservo; 
+
+const int TAMANHO = 200;
+void setup() {
+  Serial.begin(9600);
+  meuservo.attach(9);
+}
+void loop() {
+     
+  if (Serial.available() > 0) {
+    //Lê o JSON disponível na porta serial:
+    StaticJsonDocument<TAMANHO> json;
+    deserializeJson(json, Serial);
+    if(json.containsKey("grau")) {
+      int valor = json["grau"];
+      meuservo.write(valor); 
+    }
+
+  } 
+  
+}
---
+++ cRFID.ino
@@ -0,0 +1,38 @@
+MFRC522 mfrc522(SS_PIN, RST_PIN);   
+
+char st[20];
+
+const int TAMANHO = 50; 
+
+void setup() 
+{
+  Serial.begin(9600);   // Inicia a serial
+  SPI.begin();      // Inicia  SPI bus
+  mfrc522.PCD_Init();   // Inicia MFRC522
+  //Define o número de colunas e linhas do LCD:  
+}
+ 
+void loop() 
+{
+  StaticJsonDocument<TAMANHO> json;
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  String conteudo= "";
+  byte letra;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  conteudo.toUpperCase();
+  json["tag"] = conteudo;
+  serializeJson(json, Serial);
+  Serial.println();
+  delay(3000);
+} 
